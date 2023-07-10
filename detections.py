d88b4dbcdc2e6d42c6b851564755b8faaa91df08
Giuseppe Piccolo <giuseppe.piccolo@protonmail.com>
2023-07-10 16:26:05 +0200

    added methods to get maps from mongodb

--- detections.py
+++ detections.py
@@ -1,9 +1,24 @@
 from pymongo import MongoClient
+import gridfs
+import json
 
 class Detections:
     def __init__(self, uri):
         self.__client = MongoClient(uri)
         self.__db = self.__client.locations
+        self.__gridfs = gridfs.GridFS(self.__client.gridfs)
+
+    def get_Anderlecht_streets(self):
+        data = self.__gridfs.find({ "filename": "Anderlecht_streets.json" }).next().read()
+        return json.loads(data)
+    
+    def get_Bruxelles_streets(self):
+        data = self.__gridfs.find({ "filename": "Bruxelles_streets.json" }).next().read()
+        return json.loads(data)
+
+    def get_Belgium_streets(self):
+        data = self.__gridfs.find({ "filename": "Belgium_streets.json" }).next().read()
+        return json.loads(data)
 
     def get_all_detections(self, city, granularity, range):
         collections = self.__db.get_collection("{city}_{granularity}_{range}".format(city=city, granularity=granularity, range=range))
--- example_map.py
+++ example_map.py
@@ -1,13 +1,17 @@
 import folium
 import geopandas as gpd
+from detections import Detections
 
-df_anderlecht = gpd.read_file('../dataset/Anderlecht_streets.json')
+anderlecht_streets = Detections("mongodb://localhost:27017")
+data = anderlecht_streets.get_Anderlecht_streets()
+df_anderlecht = gpd.GeoDataFrame.from_features(data, data["crs"]["properties"]["name"])
 street = df_anderlecht[df_anderlecht.index.isin([0])]
 street = list(street.geometry)
-print(street[0].centroid.x, street[0].centroid.y)
+#print(street[0].centroid.x, street[0].centroid.y)
 polygons = df_anderlecht
 m = folium.Map([50.85045, 4.34878], zoom_start=13, tiles='cartodbpositron')
 tooltip = "Click me!"
 folium.Marker([street[0].centroid.y, street[0].centroid.x], popup="<i>Hey Sadas the file is here</i>", tooltip=tooltip).add_to(m)
 folium.GeoJson(polygons).add_to(m)
 m.show_in_browser()
+anderlecht_streets.close()
